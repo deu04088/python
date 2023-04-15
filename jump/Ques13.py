974ce18a5d2ececd6f44024674de338b84111020
Park_JuYoung <deu04088@naver.com>
2023-04-15 09:35:34 +0900

    4월 15일

--- jump/Ques13.py
+++ jump/Ques13.py
@@ -1,18 +1,23 @@
 def Dashinsert(a):
     list = []
     for i in a:
-        list.append(i)
-        
+        list.append(int(i))
         
     re = []
-    for i in list:
-        list = int(list)
-        re.append(i)
-        if list[i]%2 == 0 and list[i+1]%2 == 0:
-           re.append('*')
-        if list[i]%2 == 1 and list[i+1]%2 == 1:
-           re.append('-') 
+    for i in range(len(list)):
+        re.append(list[i])
+        if i < len(list)-1:
+            if list[i]%2 == 0 and list[i+1]%2 == 0:
+                re.append('*')
+            if list[i]%2 == 1 and list[i+1]%2 == 1:
+                re.append('-') 
     
-    print(re)
+    result = ''
+    for i in re:
+        if type(i) is int:
+            result += str(i)
+        else:
+            result += i
+    print(result)
     
 Dashinsert('4546793')
